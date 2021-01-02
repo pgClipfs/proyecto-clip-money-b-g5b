be757e4cd5400090f69ee248141ae8c9cf9e9adc
Facundo Ignacio Salas <56693254+1w1111876SalasFacundo@users.noreply.github.com>
2021-01-02 19:54:26 -0300

    Add files via upload
    Usuario para testeo

---
+++ InsertUsuarioPrueba.sql
@@ -0,0 +1,5 @@
+INSERT INTO CUENTA (nombre,fondo) VALUES ('Cuenta Pesos Argentinos',0)
+
+INSERT INTO Usuario (pass,nick,email,dniT,dniF,IDCuenta) VALUES ('12345678','Arturito','Arturito@gmail.com','222444','224452',1)
+
+select * from Usuario
