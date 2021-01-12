5b85323d31ab41a99ce38dbb953af6d021655dbb
Facundo Ignacio Salas <56693254+1w1111876SalasFacundo@users.noreply.github.com>
2021-01-12 19:32:55 -0300

    Update InsertUsuarioPrueba.sql
    Datos modificados para base de datos actual

--- InsertUsuarioPrueba.sql
+++ InsertUsuarioPrueba.sql
@@ -1,5 +1,5 @@
-INSERT INTO CUENTA (nombre,fondo) VALUES ('Cuenta Pesos Argentinos',0)
+insert into Cuenta (nombre,fondo) values ('ARS',100)
+insert into Usuario (pass,nick,email,nombre,apellido,dni,IDCuenta) values ('12345678','Arturito20','arturito@gmail.com','Arturito','Skywalker',1225567,1)
 
-INSERT INTO Usuario (pass,nick,email,dniT,dniF,IDCuenta) VALUES ('12345678','Arturito','Arturito@gmail.com','222444','224452',1)
+Select * from Usuario
 
-select * from Usuario
